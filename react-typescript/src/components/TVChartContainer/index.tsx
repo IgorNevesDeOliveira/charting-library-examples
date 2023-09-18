d818f8a7edbbe23c9b5a31c62085e088bfa1e425
igor-noliveira <igor.oliveira@gopuff.com>
2023-09-17 21:14:04 -0300

    extract custom indicator

--- react-typescript/src/components/TVChartContainer/index.tsx
+++ react-typescript/src/components/TVChartContainer/index.tsx
@@ -5,12 +5,13 @@ import {
 	ChartingLibraryWidgetOptions,
 	LanguageCode,
 	ResolutionString,
-	RawStudyMetaInfoId,
+	// RawStudyMetaInfoId,
 	CustomIndicator,
-	LibraryPineStudy,
-	IPineStudyResult, DeepWriteable, OhlcStudyPlotStyle, StudyPlotType, IContext
+	// LibraryPineStudy,
+	// IPineStudyResult, DeepWriteable, OhlcStudyPlotStyle, StudyPlotType, IContext
 } from '../../charting_library';
 import * as React from 'react';
+import { coloredLegStart } from '../../indicators/ColoredLegStarts';
 
 export interface ChartContainerProps {
 	symbol: ChartingLibraryWidgetOptions['symbol'];
@@ -75,186 +76,196 @@ export const TVChartContainer = () => {
 				return Promise.resolve<CustomIndicator[]>([
 
 					/* Advanced Colouring Candles */
-					{
-						name: 'Advanced Coloring Candles',
-						metainfo: {
-							_metainfoVersion: 51,
-
-							id: 'advancedcolouringcandles@tv-basicstudies-1' as RawStudyMetaInfoId,
-							name: 'Advanced Coloring Candles',
-							description: 'Advanced Coloring Candles',
-							shortDescription: 'Advanced Coloring Candles',
-
-							isCustomIndicator: true,
-
-							is_price_study: false, // whether the study should appear on the main series pane.
-							linkedToSeries: true, // whether the study price scale should be the same as the main series one.
-
-							format: {
-								type: 'price',
-								precision: 2,
-							},
-
-							plots: [
-								{
-									id: 'plot_open',
-									// tslint:disable-next-line:no-any
-									type: 'ohlc_open' as DeepWriteable<StudyPlotType.OhlcOpen>,
-									target: 'plot_candle',
-								},
-								{
-									id: 'plot_high',
-									type: 'ohlc_high' as DeepWriteable<StudyPlotType.OhlcHigh>,
-									target: 'plot_candle',
-								},
-								{
-									id: 'plot_low',
-									type: 'ohlc_low' as DeepWriteable<StudyPlotType.OhlcLow>,
-									target: 'plot_candle',
-								},
-								{
-									id: 'plot_close',
-									// tslint:disable-next-line:no-any
-									type: 'ohlc_close' as DeepWriteable<StudyPlotType.OhlcClose>,
-									target: 'plot_candle',
-								},
-								{
-									id: 'plot_bar_color',
-									type: 'ohlc_colorer' as DeepWriteable<StudyPlotType.OhlcColorer>,
-									palette: 'palette_bar',
-									target: 'plot_candle',
-								},
-								{
-									id: 'plot_wick_color',
-									type: 'wick_colorer' as DeepWriteable<StudyPlotType.CandleWickColorer>,
-									palette: 'palette_wick',
-									target: 'plot_candle',
-								},
-								{
-									id: 'plot_border_color',
-									type: 'border_colorer' as DeepWriteable<StudyPlotType.CandleBorderColorer>,
-									palette: 'palette_border',
-									target: 'plot_candle',
-								},
-							],
-
-							palettes: {
-								palette_bar: {
-									colors: [{ name: 'Colour One' }, { name: 'Colour Two' }],
-
-									valToIndex: {
-										0: 0,
-										1: 1,
-									},
-								},
-								palette_wick: {
-									colors: [{ name: 'Colour One' }, { name: 'Colour Two' }],
-
-									valToIndex: {
-										0: 0,
-										1: 1,
-									},
-								},
-								palette_border: {
-									colors: [{ name: 'Colour One' }, { name: 'Colour Two' }],
-
-									valToIndex: {
-										0: 0,
-										1: 1,
-									},
-								},
-							},
-
-							ohlcPlots: {
-								plot_candle: {
-									title: 'Candles',
-								},
-							},
-
-							defaults: {
-								ohlcPlots: {
-									plot_candle: {
-										borderColor: '#000000',
-										color: '#000000',
-										drawBorder: true,
-										drawWick: true,
-										plottype: 'ohlc_candles' as DeepWriteable<OhlcStudyPlotStyle.OhlcCandles>,
-										visible: true,
-										wickColor: '#000000',
-									},
-								},
-
-								palettes: {
-									palette_bar: {
-										colors: [
-											{ color: '#1948CC', width: 1, style: 0 },
-											{ color: '#F47D02', width: 1, style: 0 },
-										],
-									},
-									palette_wick: {
-										colors: [{ color: '#0C3299' }, { color: '#E65000' }],
-									},
-									palette_border: {
-										colors: [{ color: '#5B9CF6' }, { color: '#FFB74D' }],
-									},
-								},
-
-								precision: 2,
-								inputs: {},
-							},
-							styles: {},
-							inputs: [],
-						},
-						constructor: function (
-							this: LibraryPineStudy<IPineStudyResult>
-						) {
-
-							this.main = function (context: IContext, inputCallback) {
-								this._context = context;
-								this._input = inputCallback;
-
-								this._context.select_sym(0);
-
-								const o = PineJS.Std.open(this._context);
-								const h = PineJS.Std.high(this._context);
-								const l = PineJS.Std.low(this._context);
-								const c = PineJS.Std.close(this._context);
-
-								// Color is determined randomly
-								const colour = Math.round(Math.random());
-								return [
-									o,
-									h,
-									l,
-									c,
-									colour /*bar*/,
-									colour /*wick*/,
-									colour /*border*/,
-								];
-							};
-						},
-					},
+					coloredLegStart(PineJS) as CustomIndicator
+					// {
+					// 	name: 'Advanced Coloring Candles',
+					// 	metainfo: {
+					// 		_metainfoVersion: 51,
+					//
+					// 		id: 'advancedcolouringcandles@tv-basicstudies-1' as RawStudyMetaInfoId,
+					// 		name: 'Advanced Coloring Candles',
+					// 		description: 'Advanced Coloring Candles',
+					// 		shortDescription: 'Advanced Coloring Candles',
+					//
+					// 		isCustomIndicator: true,
+					//
+					// 		is_price_study: false, // whether the study should appear on the main series pane.
+					// 		linkedToSeries: true, // whether the study price scale should be the same as the main series one.
+					//
+					// 		format: {
+					// 			type: 'price',
+					// 			precision: 2,
+					// 		},
+					//
+					// 		plots: [
+					// 			{
+					// 				id: 'plot_open',
+					// 				// tslint:disable-next-line:no-any
+					// 				type: 'ohlc_open' as DeepWriteable<StudyPlotType.OhlcOpen>,
+					// 				target: 'plot_candle',
+					// 			},
+					// 			{
+					// 				id: 'plot_high',
+					// 				type: 'ohlc_high' as DeepWriteable<StudyPlotType.OhlcHigh>,
+					// 				target: 'plot_candle',
+					// 			},
+					// 			{
+					// 				id: 'plot_low',
+					// 				type: 'ohlc_low' as DeepWriteable<StudyPlotType.OhlcLow>,
+					// 				target: 'plot_candle',
+					// 			},
+					// 			{
+					// 				id: 'plot_close',
+					// 				// tslint:disable-next-line:no-any
+					// 				type: 'ohlc_close' as DeepWriteable<StudyPlotType.OhlcClose>,
+					// 				target: 'plot_candle',
+					// 			},
+					// 			{
+					// 				id: 'plot_bar_color',
+					// 				type: 'ohlc_colorer' as DeepWriteable<StudyPlotType.OhlcColorer>,
+					// 				palette: 'palette_bar',
+					// 				target: 'plot_candle',
+					// 			},
+					// 			{
+					// 				id: 'plot_wick_color',
+					// 				type: 'wick_colorer' as DeepWriteable<StudyPlotType.CandleWickColorer>,
+					// 				palette: 'palette_wick',
+					// 				target: 'plot_candle',
+					// 			},
+					// 			{
+					// 				id: 'plot_border_color',
+					// 				type: 'border_colorer' as DeepWriteable<StudyPlotType.CandleBorderColorer>,
+					// 				palette: 'palette_border',
+					// 				target: 'plot_candle',
+					// 			},
+					// 		],
+					//
+					// 		palettes: {
+					// 			palette_bar: {
+					// 				colors: [{ name: 'Colour One' }, { name: 'Colour Two' }],
+					//
+					// 				valToIndex: {
+					// 					0: 0,
+					// 					1: 1,
+					// 				},
+					// 			},
+					// 			palette_wick: {
+					// 				colors: [{ name: 'Colour One' }, { name: 'Colour Two' }],
+					//
+					// 				valToIndex: {
+					// 					0: 0,
+					// 					1: 1,
+					// 				},
+					// 			},
+					// 			palette_border: {
+					// 				colors: [{ name: 'Colour One' }, { name: 'Colour Two' }],
+					//
+					// 				valToIndex: {
+					// 					0: 0,
+					// 					1: 1,
+					// 				},
+					// 			},
+					// 		},
+					//
+					// 		ohlcPlots: {
+					// 			plot_candle: {
+					// 				title: 'Candles',
+					// 			},
+					// 		},
+					//
+					// 		defaults: {
+					// 			ohlcPlots: {
+					// 				plot_candle: {
+					// 					borderColor: '#000000',
+					// 					color: '#000000',
+					// 					drawBorder: true,
+					// 					drawWick: true,
+					// 					plottype: 'ohlc_candles' as DeepWriteable<OhlcStudyPlotStyle.OhlcCandles>,
+					// 					visible: true,
+					// 					wickColor: '#000000',
+					// 				},
+					// 			},
+					//
+					// 			palettes: {
+					// 				palette_bar: {
+					// 					colors: [
+					// 						{ color: '#cc1919', width: 1, style: 0 },
+					// 						{ color: '#02f416', width: 1, style: 0 },
+					// 					],
+					// 				},
+					// 				palette_wick: {
+					// 					colors: [{ color: '#cc1919' }, { color: '#02f416' }],
+					// 				},
+					// 				palette_border: {
+					// 					colors: [{ color: '#cc1919' }, { color: '#02f416' }],
+					// 				},
+					// 			},
+					//
+					// 			precision: 2,
+					// 			inputs: {},
+					// 		},
+					// 		styles: {},
+					// 		inputs: [],
+					// 	},
+					// 	constructor: function (this: LibraryPineStudy<IPineStudyResult>) {
+					// 		let coloredTimestamps: number[] = []; // Initially empty
+					//
+					// 		// Fetch data when the study is created
+					// 		fetchData().then(data => {
+					// 			coloredTimestamps = data;
+					// 		});
+					//
+					// 		this.main = function (context: IContext, inputCallback) {
+					// 			this._context = context;
+					// 			this._input = inputCallback;
+					//
+					// 			this._context.select_sym(0);
+					//
+					// 			const o = PineJS.Std.open(this._context);
+					// 			const h = PineJS.Std.high(this._context);
+					// 			const l = PineJS.Std.low(this._context);
+					// 			const c = PineJS.Std.close(this._context);
+					// 			const time = PineJS.Std.time(this._context);
+					// 			console.log(time);
+					// 			let colour;
+					// 			if (coloredTimestamps.includes(time)) {
+					// 				colour = 1;
+					// 			} else {
+					// 				colour = 0;
+					// 			}
+					//
+					// 			return [
+					// 				o,
+					// 				h,
+					// 				l,
+					// 				c,
+					// 				colour /*bar*/,
+					// 				colour /*wick*/,
+					// 				colour /*border*/,
+					// 			];
+					// 		};
+					// 	},
+					// },
 				]);
 			},
 		};
 
+		// async function fetchData() {
+		// 	return [1681738200000];
+		// }
 		const tvWidget = new widget(widgetOptions);
 		tvWidget.onChartReady(() => {
-			const studiesOnChart = tvWidget.chart().getAllStudies();
-			const isStudyAdded = studiesOnChart.some(study => study.name === 'Advanced Coloring Candles');
-			console.log('Is the study added?', isStudyAdded);
-
 			tvWidget.headerReady().then(() => {
 				const button = tvWidget.createButton();
 				button.setAttribute('title', 'Click to show a notification popup');
 				button.classList.add('apply-common-tooltip');
 				button.addEventListener('click', () => tvWidget.showNoticeDialog({
-						title: 'Notification',
-						body: 'TradingView Charting Library API works correctly',
-						callback: () => {
-							console.log('Noticed!');
-						},
-					}));
+					title: 'Notification',
+					body: 'TradingView Charting Library API works correctly',
+					callback: () => {
+						console.log('Noticed!');
+					},
+				}));
 				button.innerHTML = 'Check API';
 			});
 		});
---
+++ react-typescript/src/indicators/ColoredLegStarts.tsx
@@ -0,0 +1,181 @@
+import {
+    DeepWriteable, IContext, IPineStudyResult,
+    LibraryPineStudy,
+    OhlcStudyPlotStyle, PineJS,
+    RawStudyMetaInfoId,
+    StudyPlotType
+} from '../charting_library';
+
+export const coloredLegStart = (pineJS: PineJS) => ({
+    name: 'Advanced Coloring Candles',
+        metainfo: {
+    _metainfoVersion: 51,
+
+        id: 'advancedcolouringcandles@tv-basicstudies-1' as RawStudyMetaInfoId,
+        name: 'Colored Leg starts',
+        description: 'Colored Leg starts',
+        shortDescription: 'Colored Leg starts',
+
+        isCustomIndicator: true,
+
+        is_price_study: false, // whether the study should appear on the main series pane.
+        linkedToSeries: true, // whether the study price scale should be the same as the main series one.
+
+        format: {
+        type: 'price',
+            precision: 2,
+    },
+
+    plots: [
+        {
+            id: 'plot_open',
+            // tslint:disable-next-line:no-any
+            type: 'ohlc_open' as DeepWriteable<StudyPlotType.OhlcOpen>,
+            target: 'plot_candle',
+        },
+        {
+            id: 'plot_high',
+            type: 'ohlc_high' as DeepWriteable<StudyPlotType.OhlcHigh>,
+            target: 'plot_candle',
+        },
+        {
+            id: 'plot_low',
+            type: 'ohlc_low' as DeepWriteable<StudyPlotType.OhlcLow>,
+            target: 'plot_candle',
+        },
+        {
+            id: 'plot_close',
+            // tslint:disable-next-line:no-any
+            type: 'ohlc_close' as DeepWriteable<StudyPlotType.OhlcClose>,
+            target: 'plot_candle',
+        },
+        {
+            id: 'plot_bar_color',
+            type: 'ohlc_colorer' as DeepWriteable<StudyPlotType.OhlcColorer>,
+            palette: 'palette_bar',
+            target: 'plot_candle',
+        },
+        {
+            id: 'plot_wick_color',
+            type: 'wick_colorer' as DeepWriteable<StudyPlotType.CandleWickColorer>,
+            palette: 'palette_wick',
+            target: 'plot_candle',
+        },
+        {
+            id: 'plot_border_color',
+            type: 'border_colorer' as DeepWriteable<StudyPlotType.CandleBorderColorer>,
+            palette: 'palette_border',
+            target: 'plot_candle',
+        },
+    ],
+
+        palettes: {
+        palette_bar: {
+            colors: [{ name: 'Colour One' }, { name: 'Colour Two' }],
+
+                valToIndex: {
+                0: 0,
+                    1: 1,
+            },
+        },
+        palette_wick: {
+            colors: [{ name: 'Colour One' }, { name: 'Colour Two' }],
+
+                valToIndex: {
+                0: 0,
+                    1: 1,
+            },
+        },
+        palette_border: {
+            colors: [{ name: 'Colour One' }, { name: 'Colour Two' }],
+
+                valToIndex: {
+                0: 0,
+                    1: 1,
+            },
+        },
+    },
+
+    ohlcPlots: {
+        plot_candle: {
+            title: 'Candles',
+        },
+    },
+
+    defaults: {
+        ohlcPlots: {
+            plot_candle: {
+                borderColor: '#000000',
+                    color: '#000000',
+                    drawBorder: true,
+                    drawWick: true,
+                    plottype: 'ohlc_candles' as DeepWriteable<OhlcStudyPlotStyle.OhlcCandles>,
+                    visible: true,
+                    wickColor: '#000000',
+            },
+        },
+
+        palettes: {
+            palette_bar: {
+                colors: [
+                    { color: '#cc1919', width: 1, style: 0 },
+                    { color: '#02f416', width: 1, style: 0 },
+                ],
+            },
+            palette_wick: {
+                colors: [{ color: '#cc1919' }, { color: '#02f416' }],
+            },
+            palette_border: {
+                colors: [{ color: '#cc1919' }, { color: '#02f416' }],
+            },
+        },
+
+        precision: 2,
+            inputs: {},
+    },
+    styles: {},
+    inputs: [],
+},
+    constructor: function (this: LibraryPineStudy<IPineStudyResult>) {
+        let coloredTimestamps: number[] = []; // Initially empty
+
+        // Fetch data when the study is created
+        fetchData().then(data => {
+            coloredTimestamps = data;
+        });
+
+        this.main = function (context: IContext, inputCallback) {
+            this._context = context;
+            this._input = inputCallback;
+
+            this._context.select_sym(0);
+
+            const o = pineJS.Std.open(this._context);
+            const h = pineJS.Std.high(this._context);
+            const l = pineJS.Std.low(this._context);
+            const c = pineJS.Std.close(this._context);
+            const time = pineJS.Std.time(this._context);
+            console.log(time);
+            let colour;
+            if (coloredTimestamps.includes(time)) {
+                colour = 1;
+            } else {
+                colour = 0;
+            }
+
+            return [
+                o,
+                h,
+                l,
+                c,
+                colour /*bar*/,
+                colour /*wick*/,
+                colour /*border*/,
+            ];
+        };
+    },
+});
+
+async function fetchData() {
+    return [1681738200000];
+}
